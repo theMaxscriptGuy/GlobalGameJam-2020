ef61afae212c49a63832931ea95d1502976b3f7f
ajnaduvil <ajnaduvil@gmail.com>
2020-02-01 00:34:39 +0530

    Basic statemachine and proejct setup

---
+++ Project/GGJ2020.VR.Unity/Assets/App/Utils/GameObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameObjectExtensions
+{
+    public static void EnableAll(this GameObject[] gameObjects, bool flag)
+    {
+        foreach (GameObject go in gameObjects)
+        {
+            go.SetActive(flag);
+        }
+    }
+}
