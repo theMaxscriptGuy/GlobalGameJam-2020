25b23c4d3499e18f0cca3892d70bb66916cbf3aa
Moksh Talreja <moksh@hashstash.in>
2020-02-01 14:32:10 +0530

    WIP Touch

--- Project/Repair/Assets/Scripts/LevelOne/Piece.cs
+++ Project/Repair/Assets/Scripts/LevelOne/Piece.cs
@@ -1,8 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
     public int id;
+
+    private Vector3 offset = Vector3.zero;
+    private Vector3 screenPoint = Vector3.zero;
+    private Vector3 curScreenPoint = Vector3.zero;
+    private Vector3 curPosition = Vector3.zero;
+    
+    private void OnMouseDown()
+    {
+        Debug.Log(transform.name + "Touched");
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,screenPoint.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        Debug.Log(transform.name + "Dragged");
+        // curScreenPoint = new Vector3(Input.mousePosition.x,Input.mousePosition.y,screenPoint.z);
+        curScreenPoint = new Vector3(Input.mousePosition.x,transform.localPosition.y,Input.mousePosition.z);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);// + offset;
+        transform.position = curPosition;
+    }
+
 }
