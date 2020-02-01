9ca56e05e7b1c68c0812e12c5116d3d25fd952fa
ajnaduvil <ajnaduvil@gmail.com>
2020-02-01 18:47:28 +0530

    scene validation logic implemented

---
+++ Project/GGJ2020.VR.Unity/Assets/App/Scripts/Level1Validation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Level1Validation : MonoBehaviour
+{
+    public UnityEvent OnLevelComplete;
+
+    public int totalPieces = 6;
+    public int piecesCollected = 0;
+    public void OnPieceAttached()
+    {
+        piecesCollected++;
+        Debug.Log($"{piecesCollected} / {totalPieces} Collected!");
+
+        if (piecesCollected >= totalPieces)
+        {
+            Debug.Log($"All Pieces Collected!");
+
+            OnLevelComplete?.Invoke();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
