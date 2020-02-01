051aa3b88b76676d78451e09d2d88b397422cced
theMaxscriptGuy <videep@live.com>
2020-02-01 14:53:15 +0530

    Added glowing animation to the introduction scene.

--- Project/Repair/Assets/Scripts/LevelOne/AnimateDissolveShader.cs
+++ Project/Repair/Assets/Scripts/LevelOne/AnimateDissolveShader.cs
@@ -6,15 +6,16 @@ public class AnimateDissolveShader : MonoBehaviour
 {
     [SerializeField]
     private float speed = 0.01f;
+    private Material mat;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mat = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Renderer>().material.SetFloat("_DissolveAmount", Mathf.PingPong(Time.time * speed, 0.5f));
+        mat.SetFloat("_DissolveAmount", Mathf.PingPong(Time.time * speed, 0.5f));
     }
 }
