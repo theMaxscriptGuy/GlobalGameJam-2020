336c54b916d892ce7e0222f7492c30ea3b7d7207
theMaxscriptGuy <videep@live.com>
2020-02-02 12:39:38 +0530

    Added statistics.

---
+++ Project/Repair/Assets/Scripts/FinalLevel/RaiseExerciseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseExerciseEvent : MonoBehaviour
+{
+	public static Action doExercise;
+	private void OnTriggerStay(Collider other)
+	{
+		doExercise?.Invoke();
+	}
+}
