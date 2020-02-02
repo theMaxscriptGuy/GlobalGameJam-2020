090a301469f41c33a09755259d95d22063a4c7fa
theMaxscriptGuy <videep@live.com>
2020-02-02 11:04:07 +0530

    Added lifeline and timer.

---
+++ Project/GGJ2020.VR.Unity/Assets/Scripts/LevelOne/Lifeline.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifeline : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Material mat;
+    private float initialVal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        initialVal = mat.GetFloat("_FillAmount");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("_FillAmount", initialVal - (Time.deltaTime * speed));
+        initialVal = mat.GetFloat("_FillAmount");
+    }
+}
