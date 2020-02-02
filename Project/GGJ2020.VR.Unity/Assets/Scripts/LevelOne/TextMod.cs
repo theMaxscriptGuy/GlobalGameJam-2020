20ddc86b2cc508da0c047100605f7dec260bff9a
theMaxscriptGuy <videep@live.com>
2020-02-02 11:21:34 +0530

    added sample text mesh pro asset and sounds.

---
+++ Project/GGJ2020.VR.Unity/Assets/Scripts/LevelOne/TextMod.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextMod : MonoBehaviour
+{
+    [SerializeField] private float timeModifier = 0f;
+    [SerializeField] private float maxDilation = 0f;
+    private TextMeshPro tmp;
+    void Start()
+    {
+        tmp = GetComponent<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        tmp.fontMaterial.SetFloat(ShaderUtilities.ID_FaceDilate, Mathf.PingPong(Time.time * timeModifier,maxDilation));
+    }
+}
